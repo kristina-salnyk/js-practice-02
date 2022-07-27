edff4e4645e5afac170cc8615cd79502dfb7f443
Kristina Salnyk <Kristina.Salnyk@gmail.com>
2022-07-28 00:05:56 +0200

    Added script for 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallaryRef = document.querySelector('div.gallery');
+
+const gallaryItemsMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`
+  )
+  .join(' ');
+
+gallaryRef.innerHTML = gallaryItemsMarkup;
+
+const gallaryClickHandler = event => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') return;
+
+  const gallaryItemInstance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600" alt="${event.target.alt}">`
+  );
+
+  gallaryItemInstance.show();
+};
+
+gallaryRef.addEventListener('click', gallaryClickHandler);
